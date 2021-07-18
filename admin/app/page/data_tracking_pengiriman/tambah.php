08e374539738c59138916873b4df6d506c294639
Facetozvers <whatthefapmc@gmail.com>
2021-07-18 10:44:53 +0700

    maps search box

--- admin/app/page/data_tracking_pengiriman/tambah.php
+++ admin/app/page/data_tracking_pengiriman/tambah.php
@@ -36,8 +36,15 @@
                             </td>
                             <td width="2%">:</td>
                             <td>
-                                <input readonly class="form-control" style="width:50%" type="hidden" name="id_driver" id="id_driver" placeholder="Id Driver" value="<?php echo $_COOKIE['id']?>" required="required"></input>
-                                <input readonly class="form-control" style="width:50%" type="text" name="nama_driver" id="nama_driver" placeholder="Nama Driver" value="<?php echo $_COOKIE['name']?>" required="required"></input>
+                                <?php if($_COOKIE['siapa'] == 'driver') { ?>
+                                    <input readonly class="form-control" style="width:50%" type="hidden" name="id_driver" id="id_driver" placeholder="Id Driver" value="<?php echo $_COOKIE['id']?>" required="required"></input>
+                                    <input readonly class="form-control" style="width:50%" type="text" name="nama_driver" id="nama_driver" placeholder="Nama Driver" value="<?php echo $_COOKIE['name']?>" required="required"></input>
+                                    <?php } 
+                                else {?>
+                                <select class="form-control" style="width:50%" type="text" name="id_driver" id="id_driver" placeholder="Id Driver " required="required">
+                                <option></option><?php combo_database_v2('data_driver','id_driver','nama',''); ?>
+                                </select>
+                                <?php } ?>
                             </td>
                         </tr>
                           <tr>
@@ -50,7 +57,7 @@
                          
 <!-- MAPS INPUT -->
 <script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDeRK32JN_ZeJMRxa-iZGycriUxxxY3WZg&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDeRK32JN_ZeJMRxa-iZGycriUxxxY3WZg&callback=initMap&libraries=places">
 </script>
 <script>
     function initMap() {
@@ -60,6 +67,67 @@
             zoom: 15,
             center: myLatlng,
         });
+        // Create the search box and link it to the UI element.
+            const input = document.getElementById("pac-input");
+            const searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener("bounds_changed", () => {
+                searchBox.setBounds(map.getBounds());
+            });
+            let markers = [];
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener("places_changed", () => {
+              const places = searchBox.getPlaces();
+
+              if (places.length == 0) {
+                return;
+              }
+              // Clear out the old markers.
+              markers.forEach((marker) => {
+                marker.setMap(null);
+              });
+              markers = [];
+              // For each place, get the icon, name and location.
+              const bounds = new google.maps.LatLngBounds();
+              places.forEach((place) => {
+                if (!place.geometry || !place.geometry.location) {
+                  console.log("Returned place contains no geometry");
+                  return;
+                }
+                const icon = {
+                  url: place.icon,
+                  size: new google.maps.Size(71, 71),
+                  origin: new google.maps.Point(0, 0),
+                  anchor: new google.maps.Point(17, 34),
+                  scaledSize: new google.maps.Size(25, 25),
+                };
+                // Create a marker for each place.
+                markers.push(
+                  new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                  })
+                );
+
+                if (place.geometry.viewport) {
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+                } else {
+                  bounds.extend(place.geometry.location);
+                }
+
+                $('#latlongclicked').val(place.geometry.location);
+                $('#latspan').html(place.geometry.location.lat());
+                $('#lngspan').html(place.geometry.location.lng());
+                $('#lat').val(place.geometry.location.lat());
+                $('#lng').val(place.geometry.location.lng());
+              });
+              map.fitBounds(bounds);
+            });
        // Create the initial InfoWindow.
             let infoWindow = new google.maps.InfoWindow({
                 content: "Click the map to get Lat/Lng!",
@@ -109,6 +177,77 @@
     height: 340px;
     border: 5px solid #DEEBF2;
 }
+#description {
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+}
+
+#infowindow-content .title {
+  font-weight: bold;
+}
+
+#infowindow-content {
+  display: none;
+}
+
+#map #infowindow-content {
+  display: inline;
+}
+
+.pac-card {
+  margin: 10px 10px 0 0;
+  border-radius: 2px 0 0 2px;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  outline: none;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  background-color: #fff;
+  font-family: Roboto;
+}
+
+#pac-container {
+  padding-bottom: 12px;
+  margin-right: 12px;
+}
+
+.pac-controls {
+  display: inline-block;
+  padding: 5px 11px;
+}
+
+.pac-controls label {
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 300;
+}
+
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 400px;
+}
+
+#pac-input:focus {
+  border-color: #4d90fe;
+}
+
+#title {
+  color: #fff;
+  background-color: #4d90fe;
+  font-size: 25px;
+  font-weight: 500;
+  padding: 6px 12px;
+}
+
+#target {
+  width: 345px;
+}
 ul {
     font: 10pt arial;
     margin-left: 0;
@@ -136,6 +275,12 @@ li {
 
 <div class="main">
     <div style="width:100%; margin:0px auto;">
+            <input
+            id="pac-input"
+            class="controls"
+            type="text"
+            placeholder="Cari Lokasi..."
+            />
         <div id="map"></div>
         <div class="eventtext">
             <div>Lattitude:
